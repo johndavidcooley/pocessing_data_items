56701b5726abc4c2cdf81edb1e9f8c618505c6d6
John Cooley <johndavidcooley@gmail.com>
2016-03-04 01:53:57 -0500

    Answered more questions.
    Also made minor stylistic changes that will likely be changed later on.

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-// var items = [{}, {}, {}, {}, {}, ... {}]
-
-// Mid Century Siesta
-// Hops shirt
-// Dragon beer mug
-// Paulaner Das Rathaus
-// Bottle cap catcher
-// GUN PISTOL
-// RUSH ORDER
-// 1970s Coors
-// Medium Size Welcome
-// Follow Me Bring Beer
-// 1970s Schlitz
-// Libbey 2.5 oz. Shot Glass
-// Qty of 2 Groomsmen GIft
-// Wine Corkscrew
-// Magnetic Wall Mount
-// 20% OFF SALE
-// The Three Broomsticks
-// Engraves Pocket Knife
-// Hand Painted Colorful Feather Glass
-// Groomsman Gifts
-// Bike Beer Growler
-// 1970s PBR Glass
-// Libbey 14oz Glass
-// Libbey 14oz Glass
-// Beautiful Vintage Pendant
-// 
-
-//Stuff to look at:
-//-----title
-//-----price
-//-----currency_code
-//-----Materials
-
 var answerOne = document.querySelector('#answer1');
 var answerTwo = document.querySelector('#answer2');
 var answerThree = document.querySelector('#answer3');
@@ -42,62 +7,106 @@ var answerSix = document.querySelector('#answer6');
 
 
 // Question 1
-// Show me how to calculate the average price of all items.
+// Show me how to calculate the average price of all objects.
+//
+// We need to map [items] into a single array that includes
+// only the prices. Then we need to reduce the new array into
+// a single number equal to the sum of all prices. Then we
+// need to didve the sum of all prices by the number of
+// objects in [items].
 
 var arrayOfPrices = items.map(function(obj) {
 	return obj.price;
 });
 var totalPrice = arrayOfPrices.reduce(function(sumOfPrice, currentPrice) {
-	sumOfPrice += currentPrice;
+	return sumOfPrice += currentPrice;
 }, 0);
-var avgPrice = totalPrice / items.length;
+var avgPrice = Math.round((totalPrice / items.length) * 100) / 100;
 answerOne.textContent = "The average price is $" + avgPrice;
-
-
 
 
 // Question 2
 // Show me how to get an array of items that cost between $14.00 and $18.00 USD
+//
+// We need to filter [items] to return a new array of items
+// with prices greater than 14.00 AND less than 18.00.
+
 
 var newArray = items.filter(function(object) {
-	return object.price > 14.00 && object["price"] < 18.00;
+	return (object.price > 14.00 && object.price < 18.00);
 });
-var namesOfStuff = newArray["title"];
+var namesOfStuff = newArray.map(function(object) {
+	return object.title;
+})
 answerTwo.textContent = namesOfStuff;
 
 
 
 // Question 3
 // Which item has a "GBP" currency code? Display it's name and price.
+//
+// We need to filter [items] into an array with a single object
+// in which the object has a property of "currency_code" that
+// is equal to "GBP". Then we need to find the name and price
+// of the item in our single array.
 
 var hasGbp = items.filter(function(object) {
-	return object.currency_code === 'GBP';
+	return object.currency_code === "GBP";
 });
-var nameOfGbp = hasGbp title;
-var priceOfGbp = hasGbp.price;
-answerThree.textContent = nameOfGbp + 'costs' + priceOfGbp;
+var nameOfGbp = hasGbp.map(function(object) {
+	return object.title;
+});
+var priceOfGbp = hasGbp.map(function(object) {
+	return object.price;
+});
+var pounds = '&pound';
+answerThree.textContent = nameOfGbp + ' costs ' + pounds + priceOfGbp;
 
 
 // Question 4
 // Display a list of all items who are made of wood.
-// filter by
+//
+// We need to filter [items] into an array containing items
+// made of wood. Then we need to get the title of each
+// object in the array.
 
-
+var madeOfWood = items.filter(function(object) {
+	return object.materials === "wood";
+});
 
 
 // Question 5
 // Which items are made of eight or more materials? Display the name,
 // number of items and the items it is made of.
+//
+// We need to filter [items] into a new array consisting of
+// only object with the proprty "materials" that is at
+// least 8.
 
-
-
-
+var madeOf8 = items.filter(function(object) {
+	return object.materials.length >= 8;
+});
+var name8 = madeOf8.map(function(object) {
+	return object.title;
+})
+var items8 =  madeOf8.map(function(object) {
+	return object.materials;
+})
+var num8 = madeOf8.map(function(object) {
+	return object.materials.length;
+});
+answerFive.textContent = name8[0] + " has " + num8[0] + " materials" + name8[0] + " " + name8[1] + " has " + num8[1] + " materials" + name8[1];
 
 // Question 6
 // How many items were made by their sellers?
+//
+// We need to filter [items] into a new array consisting of
+// objects with a property of "who_made" equal to "i_did".
+// Then we need to find the length of the new array.
 
-// filter(function() {
-
-// })
-
+var madeBySeller = items.filter(function(object) {
+	return object.who_made === "i_did";
+});
+var numMadeBySeller = madeBySeller.length;
+answerSix.textContent = numMadeBySeller + " were made by their sellers"
 
